6b75e03f5a9637fef8a2ab6ec7a8f20106854dc7
Michael Szymczak <contact@michaelszymczak.com>
2019-07-04 18:59:55 +0100

    sort entries before diffing to improve detecting new entries

--- jp101/diff_to_poke.sh
+++ jp101/diff_to_poke.sh
@@ -2,4 +2,5 @@
 
 iconv -f UTF-16LE -t UTF-8 japanesepod101_mywordbank.csv -o japanesepod101_mywordbank.utf8.csv
 #cat japanesepod101_mywordbank.utf8.csv| tr ',' '\t' | cut -f1,2,4 | tail -n +2 > poke.txt
-git diff japanesepod101_mywordbank.utf8.csv | grep "^+[^+]" | sed 's/\+//' | tr ',' '\t' | cut -f1,2,4 > poke.txt
+cat japanesepod101_mywordbank.utf8.csv | sort > japanesepod101_mywordbank.utf8.sorted.csv
+git diff japanesepod101_mywordbank.utf8.sorted.csv | grep "^+[^+]" | sed 's/\+//' | tr ',' '\t' | cut -f1,2,4 > poke.txt
